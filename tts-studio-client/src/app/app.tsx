321e573405a601a909f6a5e55a9d39891b10b5fb
Matthias Gaudin <gatthias@gmail.com>
2023-06-11 14:39:05 +0200

    chore: connect with colab backend

--- tts-studio-client/src/app/app.tsx
+++ tts-studio-client/src/app/app.tsx
@@ -1,6 +1,7 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { observer } from "mobx-react-lite";
-import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
-import { AudioSourceStore, SourceBatch, SourceSegment } from "../stores/AudioSourceStore";
+import { createContext, PropsWithChildren, useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { AudioSourceStore, SourceBatch, SourceSegment, SERVER_URL } from "../stores/AudioSourceStore";
 
 interface AppContext {
   audioSourceStore: AudioSourceStore
@@ -16,6 +17,30 @@ export const useStores = () => {
   return useContext(AppContext);
 };
 
+const timeToString = (time: number) => {
+  return Math.floor(time / 1000 * 10) / 10;
+}
+
+interface ButtonProps extends PropsWithChildren {
+  loading?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({ onClick, loading = false, children }: ButtonProps) => {
+  const className = `bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded`;
+  
+  return (
+    <button className={className} onClick={() => onClick?.()} disabled={!!loading}>
+      <div className="flex justify-center items-center">
+        { loading && <div><ArrowPathIcon className="animate-spin w-4 h-4 mr-2"/></div>}
+        <div>
+          { children }
+        </div>
+      </div>
+    </button>
+  )
+}
+
 
 const App = observer((): JSX.Element => {
   const { audioSourceStore } = useStores();
@@ -24,35 +49,108 @@ const App = observer((): JSX.Element => {
     return <span>Loading...</span>;
   }
 
-  const [batchUid] = Object.keys(audioSourceStore.source.batches);
-  const batch = audioSourceStore.source.batches[batchUid as any];
+  const batches = audioSourceStore.source.batches
+  const sourceSegments = audioSourceStore.source.segments;
 
-  const segments = Object.values(audioSourceStore.source.segments).filter(segment => segment.batch === batch.uid);
-  
+  // TODO: dataset stats (words count, durations avg & deviation, ...)
+
   return (
     <div className="flex flex-col">
-      <div>
-        <Batch batch={batch} segments={segments} />
+      <div className="p-2">
+          <div className="overflow-x-auto bg-slate-300 rounded">
+              <div style={{ width: `${batches.length * 512}px` }} className="flex">
+                  {
+                      batches.map(batch => {
+                          const segments = sourceSegments.filter(segment => segment.batch === batch.uid);
+
+                          return <Batch batch={batch} segments={segments} key={batch.uid} />
+                      })
+                  }
+              </div>
+          </div>
       </div>
-      <div className="p-4">
-        { audioSourceStore.selectedSegment && <SegmentCard segment={audioSourceStore.selectedSegment} /> }
+      <div className="flex justify-center">
+        { audioSourceStore.selectedSegment && (
+            <div className="max-w-lg">
+              <div className="flex justify-between p-2">
+                <Button onClick={() => audioSourceStore.selectPreviousSegment()}>Prev</Button>
+                <Button onClick={() => audioSourceStore.selectNextSegment()}>Next</Button>
+              </div>
+              <SegmentCard segment={audioSourceStore.selectedSegment} />
+            </div>
+        ) }
       </div>
     </div>
   )
 });
 
-const SegmentCard = ({ segment }: { segment: SourceSegment }) => {
+const SegmentCard = observer(({ segment }: { segment: SourceSegment }) => {
   const { audioSourceStore } = useStores();
   const batch = useMemo(() => audioSourceStore.getBatch(segment.batch), [segment.batch]);
+  const [saving, setSaving] = useState(false);
+  const [audioTimestamp, setAudioTimestamp] = useState(Date.now())
+
+  const OFFSET_START=0.3;
+  const OFFSET_END=-0.2;
+  const FADE_ALPHA=0.9;
+
+  const start = segment['start'] % (300 * 1000);
+  const end = segment['end'] % (300 * 1000);
+  const duration = batch['duration'];
+
+  const x = start / duration * 512;
+  const y = 0;
+  const width = (end - start) / duration * 512;
+  const height = 128;
+
+  const scale = 256 / width;
+  const pan = x - width / 2;
+
+  const x_fade_start = x + (OFFSET_START * FADE_ALPHA * 1000 / duration * 512)
+  const x_fade_end = x + width + (OFFSET_END * FADE_ALPHA * 1000 / duration * 512)
+
+  const saveSegment = useCallback(async () => {
+    setSaving(true);
+    await audioSourceStore.updateSegmentSlice(segment.uid);
+    setSaving(false);
+    setAudioTimestamp(Date.now())
+  }, [segment]);
 
   return (
-    <div className="p-2 rounded shadow-md max-w-lg">
-      <div>
+    <div className="p-2 rounded shadow-md bg-zinc-800 w-[512px]">
+      <div className="bg-slate-300 rounded">
+        <svg viewBox="0 0 512 128" focusable="false" width="100%">
+          <g transform={`scale(${scale} 1) translate(${-pan} 0)`}>
+            <path d={batch.envelope_path} stroke="none" fill="black" opacity="0.7"/>
+            <rect x={x} y={y} width={width} height={height} opacity="0.5" fill="slate"/>
+
+            <line x1={x} y1="128" x2={x_fade_start} y2="0" stroke="slate" strokeWidth="0.05"/>
+            <line x1={x + width} y1="128" x2={x_fade_end} y2="0" stroke="slate" strokeWidth="0.05"/>
+          </g>
+        </svg>
+      </div>
+      <div className="p-2">
+        <span className="text-sm">duration: {timeToString(segment.duration)}s [{timeToString(segment.start)}s - {timeToString(segment.end)}s]</span>
+      </div>
+      <div className="p-2">
+        <audio controls={true} src={`${SERVER_URL}/segment/${segment.uid}/wav?t=${audioTimestamp}`} preload="none" />
+      </div>
+      <div className="p-2">
         {segment.text}
+      </div>
+      <div className="p-2 text-slate-900">
+        <input className="bg-zinc-300 rounded" type="number" value={segment.start} onChange={(e: any) => audioSourceStore.setSegmentStart(segment.uid, Number(e.target.value))} step="100"/>
+        <input className="bg-zinc-300 rounded" type="number" value={segment.end} onChange={(e: any) => audioSourceStore.setSegmentEnd(segment.uid, Number(e.target.value))} step="100"/>
+      </div>
+      <div className="p-2">
+        <span className="text-sm">uid: {segment.uid}</span>
+      </div>
+      <div className="p-2">
+        <Button onClick={saveSegment} loading={saving}>Save</Button>
       </div>
     </div>
   )
-}
+})
 
 const Batch = ({ batch, segments }: { batch: SourceBatch, segments: SourceSegment[] }) => {
   return (
@@ -79,9 +177,11 @@ const Segment = ({ batch, segment }: { batch: SourceBatch, segment: SourceSegmen
   const width = (end - start) / duration * 512;
   const height = 128;
 
+  const className = `${audioSourceStore.selectedSegment?.uid === segment.uid ? 'opacity-80 hover:opacity-90' : 'opacity-50 hover:opacity-80'} fill-slate-500 stroke-slate-600`;
+
   return (
     <g onClick={selectSegment}>
-      <rect x={x} y={y} width={width} height={height} className="fill-red-500 stroke-red-600 opacity-50 hover:opacity-80"/>
+      <rect x={x} y={y} width={width} height={height} className={className}/>
     </g>
   );
 }
--- tts-studio-client/src/stores/AudioSourceStore.ts
+++ tts-studio-client/src/stores/AudioSourceStore.ts
@@ -1,15 +1,21 @@
-import { action, computed, makeObservable, observable } from "mobx";
+import { action, computed, makeObservable, observable, runInAction } from "mobx";
 import fakeData from "../fake_data/metadata.json"
+
+export const SERVER_URL: string = 'https://34ee-34-32-215-40.ngrok-free.app';
 
 interface IndexMap<T> {
     [uid: string]: T;
 }
 
+export type AudioSegmentStatus = 'UNCHECKED' | 'OK' | 'WARNING' | 'ERROR' | 'DISABLED';
+
 export interface DatasetSource {
     source_name: string;
     language: string;
-    batches: IndexMap<SourceBatch>;
-    segments: IndexMap<SourceSegment>;
+    batches: SourceBatch[];
+    segments: SourceSegment[];
+    batches_index: IndexMap<number>;
+    segments_index: IndexMap<number>;
 }
 
 export interface AudioSlice {
@@ -29,41 +35,152 @@ export interface SourceSegment extends AudioSlice {
     text: string;
     words: any;
     batch: string;
+    num: number;
+    status: AudioSegmentStatus;
 }
 
 export class AudioSourceStore  {
  
-    @observable source: DatasetSource | null = fakeData as unknown as DatasetSource;
+    @observable source: DatasetSource | null = null;//fakeData as unknown as DatasetSource;
     @observable selectedSegment: SourceSegment | null = null;
  
     constructor() {
         makeObservable(this);
+
+        this.init();
+    }
+
+    async init() {
+        const data = await fetch(`${SERVER_URL}/data`).then(res => res.json());
+
+        runInAction(() => this.source = data);
     }
 
     @action selectSegment(uid:  string)  {
-        const segment = this.source!.segments[uid as string];
+        const segment = this.getSegment(uid);
         
         this.selectedSegment = segment ?? null;
     }
+
+    @action selectNextSegment() {
+        if (!this.selectedSegment) {
+            return;
+        }
+
+        const orderedSegments = this.source!.segments;
+        const currentIndex = this.selectedSegment.num;
+
+        const newIndex = (currentIndex + 1) % orderedSegments.length;
+
+        this.selectedSegment = orderedSegments[newIndex];
+    }
+
+    @action selectPreviousSegment() {
+        if (!this.selectedSegment) {
+            return;
+        }
+
+        const orderedSegments = this.source!.segments;
+        const currentIndex = this.selectedSegment.num;
+
+        const newIndex = (orderedSegments.length + currentIndex - 1) % orderedSegments.length;
+
+        this.selectedSegment = orderedSegments[newIndex];
+    }
+
+    @action setSegmentStart(uid: string, time: number) {
+        const segment = this.getSegment(uid);
+        segment.start = time;
+    }
+
+    @action setSegmentEnd(uid: string, time: number) {
+        const segment = this.getSegment(uid);
+        segment.end = time;
+    }
     
     getBatch(uid: string): SourceBatch {
-        const batch = this.source!.batches[uid as string];
+        const batchIndex: number | undefined = this.source!.batches_index[uid as string];
+
+        if (batchIndex === undefined) {
+            throw new Error(`Unknown batch ${uid}`)
+        }
+        const batch = this.source!.batches[batchIndex];
 
         if (!batch) {
-            throw new Error(`Unknown batch ${uid}`)
+            throw new Error(`Batch data not found ${uid}`)
         }
 
         return batch;
     }
 
     getSegment(uid: string): SourceSegment {
-        const segment = this.source!.segments[uid as string];
+        const segmentIndex: number | undefined = this.source!.segments_index[uid as string];
+
+        if (segmentIndex === undefined) {
+            throw new Error(`Unknown segment ${uid}`)
+        }
+
+        const segment = this.source!.segments[segmentIndex];
 
         if (!segment) {
-            throw new Error(`Unknown batch ${uid}`)
+            throw new Error(`Segment data not found ${uid}`)
         }
 
         return segment;
+    }
+
+    @action async updateSegmentSlice(uid: string) {
+        const segment = this.getSegment(uid);
+
+        const options = {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                start: segment.start,
+                end: segment.end,
+                text: segment.text,
+            })
+        }
+
+        const updated = await fetch(`${SERVER_URL}/segment/${uid}/slice`, options).then(res => res.json())
+
+        runInAction(() => {
+            this.source!.segments[segment.num] = updated;
+            
+            if (this.selectedSegment?.uid === uid) {
+                this.selectedSegment = this.source!.segments[segment.num];
+            }
+        });
+
+        return updated;
+    }
+
+    @action async updateSegmentStatus(uid: string) {
+        const segment = this.getSegment(uid);
+
+        const options = {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                status: segment.status,
+            })
+        }
+
+        const updated = await fetch(`${SERVER_URL}/segment/${uid}/status`, options).then(res => res.json())
+
+        runInAction(() => {
+            this.source!.segments[segment.num] = updated;
+            
+            if (this.selectedSegment?.uid === uid) {
+                this.selectedSegment = this.source!.segments[segment.num];
+            }
+        });
+
+        return updated;
     }
  
     // @computed get listLength()  {
